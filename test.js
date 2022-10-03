8d7774ce92e74084ce9a0f16bd448a4a1d8ea5b0
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-10-03 22:34:20 +0900

    just a testing

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-console.log('Hello Wolrd');
+const name = 'BS';
+let age = 25;
+const hasHobby = true;
+
+const summarUser = function(userName, userAge, userHasHobby){
+    return (
+        'Name is ' + 
+        userName + 
+        ', Age is ' + 
+        userAge + 
+        ' and user got hobby : ' + 
+        userHasHobby
+    );
+};
+
+console.log(summarUser(name, age, hasHobby));
